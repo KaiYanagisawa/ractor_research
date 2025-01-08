241985189563733f3c62a2983fdd0cb8d92bb514
KaiYanagisawa <12730ky@gmail.com>
2025-01-08 15:54:29 +0900

    wip: custom cop RactorSharingVariables

---
+++ lib/rubocop/cop/style/ractor_sharing_variables.rb
@@ -0,0 +1,45 @@
+require 'rubocop'
+require_relative './ractor_checker'
+
+module RuboCop
+  module Cop
+    module Style
+      class RactorSharingVariables < Base
+        MSG = 'Ractor.receive detected in the Ractor block ' \
+              'but no corresponding `%<ractor>s`.send found.'.freeze
+
+        def_node_search :ractor_new?, <<~PATTERN
+          (lvasgn $_ractor_name
+            (block
+              (send (const nil? :Ractor) :new)
+              ...
+            )
+          )
+        PATTERN
+
+        def on_lvasgn(node)
+          return unless ractor_new?(node)
+
+          p node
+
+          p node.children[0]
+
+          file_path = processed_source.file_path
+          checker = RactorChecker.new(file_path)
+          checker.check
+
+          return if checker.receive_paired_with_send?(node)
+
+          message = message(node)
+          add_offense(node, message: message)
+        end
+
+        private
+
+        def message(node)
+          format(MSG, ractor: node.children[0])
+        end
+      end
+    end
+  end
+end
