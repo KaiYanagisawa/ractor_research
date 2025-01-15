a8c9758ef00fb319323275789a87a6d043cc0fa7
KaiYanagisawa <12730ky@gmail.com>
2025-01-15 23:21:34 +0900

    wip: RactorReceiveSend autocorrect prototype

--- lib/rubocop/cop/style/ractor_receive_send.rb
+++ lib/rubocop/cop/style/ractor_receive_send.rb
@@ -5,8 +5,23 @@ module RuboCop
   module Cop
     module Style
       class RactorReceiveSend < Base
+        extend AutoCorrector
+
         MSG = 'Found `%<ractor>s`.send ' \
               'but no corresponding Ractor.receive in ractor block found.'.freeze
+
+        def_node_search :ractor_new_block?, <<~PATTERN
+          (lvasgn $_ractor_name
+            (block
+              (send (const nil? :Ractor) :new)
+              ...
+            )
+          )
+        PATTERN
+
+        def_node_search :ractor_new?, <<~PATTERN
+          (send (const nil? :Ractor) :new)
+        PATTERN
 
         def_node_search :ractor_send?, <<~PATTERN
           (send (lvar _) :send ...)
@@ -20,12 +35,31 @@ module RuboCop
           checker.check
 
           return if checker.send_paired_with_receive?(node)
+          p find_ractor_new_block(node).children[1].children[2]
 
           message = message(node)
-          add_offense(node, message: message)
+          add_offense(node, message: message) do |corrector|
+            corrector.insert_after(find_ractor_new_block(node).children[1].children[2], "\nRactor.receive")
+          end
         end
 
         private
+
+        def find_ractor_new_block(node)
+          node.each_ancestor.find { |ancestor| ractor_new_block?(ancestor) }
+              .each_child_node.find { |ancestor_node| ractor_new_block?(ancestor_node) }
+        end
+
+        def find_ractor_new_nodes(node)
+          return [] unless node.is_a?(Parser::AST::Node)
+
+          if node.type == :lvasgn &&
+             node.to_json.include?('(const nil :Ractor) :new')
+            return node
+          end
+
+          node.children.flat_map { |child| find_ractor_new_nodes(child) }
+        end
 
         def message(node)
           format(MSG, ractor: node.children[0].children[0])
